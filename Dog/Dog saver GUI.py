41703a6f61c2e8f242ac3b891166ac257a6357fc
Hubert <hubert.jgr@gmail.com>
2026-01-15 09:04:16 +0000

    Continue working on GUI version
    Added local compatibility for JSON file loading function

--- Dog/Dog saver GUI.py
+++ Dog/Dog saver GUI.py
@@ -1,14 +1,23 @@
-import tkinter as tk
-import json
-root = tk.Tk()
+import tkinter as tk  # Needed for GUI creation
+import json  # Needed to import the database
+
+
+root = tk.Tk()  # Defining root parameters
+root.config(bg="gray")
 root.title("Dog Database Saver")
 root.geometry("400x300")
 
+name = tk.Entry(root)
 
+name.pack()
 def get_database():
     try:
-        with open("./Dog/Stored_dogs.json", "r") as file:
+        with open("./Dog/Stored_dogs.json", "r") as file:  # works in GH codespace
             data = json.load(file)
+            print("data loaded!")
             return data
     except:
-        return {}
+        with open("Stored_dogs.json", "r") as file:  # works locally
+            data = json.load(file)
+            print("data loaded!")
+            return data
