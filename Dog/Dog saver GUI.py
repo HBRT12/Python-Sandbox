12d59d9cdf7da195c07c72d449f5ff72bad3426f
Hubert <hubert2recovery1@outlook.com>
2026-01-24 17:54:14 +0000

    Fully functional GUI version

--- Dog/Dog saver GUI.py
+++ Dog/Dog saver GUI.py
@@ -8,19 +8,19 @@ def get_database():
     try:
         with open("./Dog/Stored_dogs.json", "r") as file:  # works in GH codespace
             data = json.load(file)
-            print("data loaded1!")
+            print("data loaded!")
             return data
-    except:
-        with open("Stored_dogs.json", "r") as file:  # works locally
+    except:  # works in local machine
+        with open("Stored_dogs.json", "r") as file:
             data = json.load(file)
-            print("data loaded2!")
+            print("data loaded!")
             return data
 
-def confirm(user_inputs):
+def confirm(user_inputs):  # Confirmation window before submitting data
     root.withdraw()  # Hides root window
     confirmation_window = tk.Toplevel(root)
     confirmation_window.geometry("330x200")
-    info_display = tk.Label(confirmation_window,
+    info_display = tk.Label(confirmation_window,  # Displaying user inputs for confirmation
                             text=f"""You have entered the following data:
     Name: {user_inputs[0]}
     Age: {user_inputs[1]}
@@ -40,14 +40,26 @@ def confirm(user_inputs):
                           font=("Arial", 20),
                           command= lambda: confirm_no(confirmation_window, root)).grid(row=1, column=2)
 
-def confirm_no(confirmwindow, rootwindow):
-    confirmwindow.destroy()
+def confirm_no(confirmwindow, rootwindow):  # If user selects no on confirmation window
+    confirmwindow.destroy()  # Close confirmation window and return to root
     rootwindow.deiconify()
 
-def confirm_yes(confirmwindow, rootwindow, userinfo, database):
+def confirm_yes(confirmwindow, rootwindow, userinfo, database):  # If user selects yes on confirmation window
+    confirmwindow.destroy()  # Close confirmation window
     try:
-        with open("Stored_dogs.json", "w") as file:
+        with open("./Dog/Stored_dogs.json", "w") as file:
+            database[str(userinfo[6])] = {  # Adding new entry to database
+                "Name": userinfo[0],
+                "Age": userinfo[1],
+                "Age in dog years": userinfo[2],
+                "Breed": userinfo[3],
+                "Fur type": userinfo[4],
+                "Privacy": userinfo[5]
+            }
+            json.dump(database, file, indent=4)  # Saving updated database
             messagebox.showinfo("Response saved!", f"Your response was saved successfully. You may now close the program or submit another response. Response ID: {userinfo[6]}")
+            confirmwindow.destroy()
+            rootwindow.deiconify()
     except:
         messagebox.showerror("Error: Failed to save", "The program has failed to upload your data. Your response hasn't been saved!")
     
@@ -82,8 +94,8 @@ privacy = tk.Checkbutton(root,  # Checkbox to indicate if data is to be kept pri
 enter = tk.Button(root,
                   text="Submit",
                   command= lambda: confirm([dog_name.get().capitalize(),
-                                            int(dog_age.get()) if dog_age.get().isdigit() else "No age provided",
-                                            int(dog_age.get())*7 if dog_age.get().isdigit() else "N/A",
+                                            int(dog_age.get()) if dog_age.get().isdigit() else "No age provided",  # Dog age validation
+                                            int(dog_age.get())*7 if dog_age.get().isdigit() else "N/A",  # Dog age in dog years validation
                                             dog_breed.get().capitalize(),
                                             dog_fur_type.get().capitalize(), 
                                             dog_privacy.get(),
