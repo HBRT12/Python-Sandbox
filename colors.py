e85517b6df86bb5002a0edb2faa48ae0eb47abe4
Hubert <hubert2recovery1@outlook.com>
2026-01-28 19:56:34 +0000

    Delete unnecessary files

--- colors.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.colors as mcolors
-
-# Get all named colors
-named_colors = mcolors.CSS4_COLORS
-
-# Display all colors
-for color_name, hex_value in named_colors.items():
-    print(f"{color_name}: {hex_value}")
