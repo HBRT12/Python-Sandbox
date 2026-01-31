819644093e90222dc9a5066c74d0930ca864c503
Hubert <hubert2recovery1@outlook.com>
2026-01-31 18:34:44 +0000

    Refactor JSON reader for improved readability

--- json learning/JSON reader.py
+++ json learning/JSON reader.py
@@ -1,12 +1,11 @@
 import json
-import time
 
-with open('my first json file.json','r') as file:
+with open('my first json file.json','r') as file:  # Imports saved dictionary
     dictionary_1 = json.load(file)
 try:
     print(f'Hello, {dictionary_1["name"]}! your date of birth is {dictionary_1["day of birth"]} {dictionary_1["month of birth"][:3]} {dictionary_1["year of birth"]} and you are {dictionary_1["age"]} years old!')
-    print('JSON reading successful!')
+    print('JSON reading successful!')  # Displays saved data from dictionary
 except:
     print('ERROR: Failed to load JSON file content')
 
-input('\n\nPress enter to exit...')
+input('\n\nPress enter to exit...')  # Stops program from closing immediately
