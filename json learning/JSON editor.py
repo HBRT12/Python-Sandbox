0798539e77916eef67b895ee7e9b2b83c0921291
Hubert <hubert2recovery1@outlook.com>
2026-01-31 18:40:41 +0000

    Refactor JSON editor for improved user input handling
    refactored variables, commented and spaced out parts of the code

--- json learning/JSON editor.py
+++ json learning/JSON editor.py
@@ -1,21 +1,21 @@
-import json
-import time
-dictionary_1 = {'name': None,
-                'day of birth': None,
-                'month of birth': None,
-                'year of birth': None,
-                'age': None}
+import json  # Needed to save data
+
+
+dictionary_1 = {}  # Creates dictionary
+
 name = input('What is your name>>> ')
-dictionary_1['name']=name
-dob = input('What day were you born on>>> ')
-dictionary_1['day of birth']=dob
-mob = input('What month were you born in>>> ')
-dictionary_1['month of birth']=mob
-yob = input('What year were you born in>>> ')
-dictionary_1['year of birth']=yob
+dictionary_1['name'] = name
+day_of_birth = input('What day were you born on>>> ')
+dictionary_1['day of birth'] = day_of_birth
+month_of_birth = input('What month were you born in>>> ')  # Gets user info
+dictionary_1['month of birth'] = month_of_birth
+year_of_birth = input('What year were you born in>>> ')
+dictionary_1['year of birth'] = year_of_birth
 age = input('How old are you>>> ')
-dictionary_1['age']=age
-with open('my first json file.json','w') as file:
+dictionary_1['age'] = age
+
+with open('my first json file.json','w') as file:  # saves user info
     json.dump(dictionary_1, file, indent=4)
 print('Saved to JSON file!')
-time.sleep(1)
+
+input("Press enter to exit")  # Stops program from closing instantly
