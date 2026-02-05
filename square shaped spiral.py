8c2444374d9b5b7fbe98b9b8f2c8e4855f1405df
Hubert <hubert2recovery1@outlook.com>
2026-02-05 08:55:29 +0000

    Add random colours & wider pen

--- square shaped spiral.py
+++ square shaped spiral.py
@@ -1,10 +1,19 @@
 import turtle as t
-
+import random as r
 
 def main():
+    colours = ["yellow", "red", "blue", "green", "yellow", "orange", "purple", "pink"]
     ITERATIONS = 50
+    s=t.Screen()
+    s.bgcolor("gray")
     trt = t.Turtle()
     trt.speed(0)
+    trt.width(20)
     for i in range(ITERATIONS):
+        trt.color(r.choice(colours))
         trt.forward(i * 10)
         trt.right(90)
+
+
+if __name__ == "__main__":
+    main()
