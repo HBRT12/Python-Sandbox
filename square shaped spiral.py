2c0312e7f2c5780713d27af8ab8c7cc597a65504
Hubert <hubert.jgr@gmail.com>
2026-02-08 13:10:35 +0000

    Remove some colors, add RANDOM_COLOUR bool

--- square shaped spiral.py
+++ square shaped spiral.py
@@ -2,15 +2,19 @@ import turtle as t
 import random as r
 
 def main():
-    colours = ["yellow", "red", "blue", "green", "yellow", "orange", "purple", "pink"]
+    colours = ["red","blue","green","yellow"]
     ITERATIONS = 50
+    RANDOM_COLOURS = True
     s=t.Screen()
     s.bgcolor("gray")
     trt = t.Turtle()
     trt.speed(0)
     trt.width(20)
     for i in range(ITERATIONS):
-        trt.color(r.choice(colours))
+        if RANDOM_COLOURS:
+            trt.color(r.choice(colours))
+        else:
+            trt.color(colours[i % 4])
         trt.forward(i * 10)
         trt.right(90)
 
