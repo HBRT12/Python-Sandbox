5d3a4585644a15ac5f4ca11b1a64fe3afd39ec59
Hubert <hubert2recovery1@outlook.com>
2026-02-09 20:50:59 +0000

    Add comments for clarity in greeter.py

--- Greeter/greeter.py
+++ Greeter/greeter.py
@@ -1,12 +1,12 @@
-import json
+import json  # Needed for saving/loading names
 
 def main(name):
-    if name.lower() == "hubert":
+    if name.lower() == "hubert":  # Checks if name matches my cool name
         print(f"hello, Hubert. Welcome back to your coding sandbox!")
     else:
         print(f"Hello, {name}. Welcome to this Python program")
 
 name_var = input("Please enter your name")
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # Ensures program is run directly
     main(name_var)
