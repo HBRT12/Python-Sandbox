a3e04b1f5e803f06debbbd2e6069fb2ea1fd22da
Hubert <hubert2recovery1@outlook.com>
2026-02-10 22:00:45 +0000

    Add comments to square shaped spiral.py for clarity

--- square shaped spiral.py
+++ square shaped spiral.py
@@ -1,23 +1,23 @@
-import turtle as t
-import random as r
+import turtle as t  # Used for graphics
+import random as r  # Used for randomisation
 
 def main():
-    colours = ["red","blue","green","yellow"]
+    colours = ["red","blue","green","yellow"]  # Colours used in spiral
     ITERATIONS = 50
     RANDOM_COLOURS = True
-    s=t.Screen()
+    s=t.Screen()  # Create screen
     s.bgcolor("gray")
-    trt = t.Turtle()
+    trt = t.Turtle()  # Create turtle
     trt.speed(0)
     trt.width(20)
     for i in range(ITERATIONS):
         if RANDOM_COLOURS:
             trt.color(r.choice(colours))
         else:
-            trt.color(colours[i % 4])
+            trt.color(colours[i % 4])  # Keeps colours the same on each side
         trt.forward(i * 10)
-        trt.right(90)
+        trt.right(90)  # Move and rotate
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # Prevents running from being imported
     main()
