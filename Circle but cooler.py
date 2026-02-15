5c3cc74cdb8b4955bde77a9c089356a82099145e
Hubert <hubert2recovery1@outlook.com>
2026-02-15 19:31:41 +0000

    Enhance Circle drawing script with comments
    Added comments for clarity and defined parameters for the turtle graphics.

--- Circle but cooler.py
+++ Circle but cooler.py
@@ -1,16 +1,18 @@
-import turtle as poopookakaland
+import turtle as poopookakaland  # Graphics
 import random as r
 
+
 colours = ["red","orange","yellow","green","blue","cyan","teal","brown","pink","gray"]
+
 scr = poopookakaland.Screen()
-trt = poopookakaland.Turtle()
+trt = poopookakaland.Turtle()  # Defining parameters for screen
 trt.width(3)
 
 trt.speed(0)
-scr.bgcolor("white")
+scr.bgcolor("white")  # Defining colours for starting
 trt.color("black")
 
-for i in range(360):
+for i in range(360):  # The main thing
     trt.circle(180-i/2)
     trt.right(0.5)
     trt.color(r.choice(colours))
