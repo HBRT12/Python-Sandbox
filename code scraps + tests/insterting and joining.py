9eebcf07e1778f14203e39609e5104b2175f81cc
Hubert <hubert.jgr@gmail.com>
2026-02-22 23:55:42 +0000

    Update insterting and joining.py

--- code scraps + tests/insterting and joining.py
+++ code scraps + tests/insterting and joining.py
@@ -1,6 +1,6 @@
 nums = ["1","2","3","4","5","6"]
-nums.insert(2, "-") #1,2,-,3,4,5,6
-nums.insert(5, "-")
+nums.insert(2, "-")  # puts - at index 2 without replacing
+nums.insert(5, "-")  # puts - at index 5 without replacing
 print(nums)
-jnums = ''.join(nums)
+jnums = ''.join(nums)  # converts list into one string with nothing between each value
 print(jnums)
